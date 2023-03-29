284cdbbd00f8b1bafb5ca686b842f093514a6b6a
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-03-29 06:54:32 +0300

    Exercise and exercise_log models

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,8 @@
 class Exercise < ApplicationRecord
   belongs_to :workout
-  has_many :exercise
+  has_many :exercise_logs
+
+  validates :name, presence: true
+  validates :description, presence: true, length: { minimum: 20 }
+  validates :calories, presence: true, numericality: {only_integer: true}
 end
