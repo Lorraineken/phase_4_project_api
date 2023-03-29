bbb8ef31820bf822d6191bc9625cb5de1c007210
Lorraineken <lorrainekupa@gmail.com>
2023-03-29 05:31:20 +0300

    Added show action in workout

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,10 +1,25 @@
 class WorkoutsController < ApplicationController
    
+    def create 
+        workout = Workout.create!(workout_params)
+        render json: workout
+    end
+
     def index 
         workout = Workout.all 
         render json: workout
     end
 
-    
+    def show 
+        workout = Workout.find(params[:id])
+        render json: workout
+    end
+
+
+   private 
+
+   def workout_params 
+    params.permit(:name, :weight, :date, :category)
+   end
 
 end
