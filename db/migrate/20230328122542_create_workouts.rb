7f10ead7aecb84fb63a6487aed4e2fe475e773b4
Lorraineken <lorrainekupa@gmail.com>
2023-03-29 11:23:42 +0300

    Added instructor column to workout

--- db/migrate/20230328122542_create_workouts.rb
+++ db/migrate/20230328122542_create_workouts.rb
@@ -3,7 +3,7 @@ class CreateWorkouts < ActiveRecord::Migration[7.0]
     create_table :workouts do |t|
       t.string :name, null:false
       t.boolean :weight, null:false
-      t.date :date, null:false
+      t.string :instructor, null:false
       t.string :category, null:false
 
       t.timestamps
