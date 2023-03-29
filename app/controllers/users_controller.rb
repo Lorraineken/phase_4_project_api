db79cbf35f6573b06b1eb0684e9cc86a9ff45e43
Lorraineken <lorrainekupa@gmail.com>
2023-03-29 05:00:44 +0300

    Added index action for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+    def index 
+        user = User.create(user_params)
+        render json:user
+    end
+
+    
+    private 
+
+    def user_params 
+        params.permit(:user_name, :full_name, :password, :email)
+    end
+
 end
