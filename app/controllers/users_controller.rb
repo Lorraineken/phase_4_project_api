923e7a42d7a4351807e18862c06c44f7ff48eccf
Lorraineken <lorrainekupa@gmail.com>
2023-03-29 09:05:08 +0300

    Added create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
-    def index 
+    def create 
         user = User.create!(user_params)
+        render json: user
+    end
+
+    def index 
+        user = User.all 
         render json: user
     end
 
