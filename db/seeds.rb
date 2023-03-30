e91a247fa0ea6a33a4f21d8ce316f98cd48fd17c
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-03-30 10:24:37 +0300

    Workout seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,22 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+workout_names = [  "CrossFit",  "Spin class",  "Yoga",  "Pilates",  "Zumba",  "Barre",  "Kickboxing",  "HIIT",  "Bootcamp",  "Boxing",  "Running",  "Swimming",  "Cycling",  "Rowing",  "Weightlifting"]
+
+15.times do
+  Workout.create!(
+    name: workout_names.sample,
+    weight: [true, false].sample,
+    instructor: Faker::Name.name,
+    category: %w[Cardio Strength Yoga].sample
+  )
+end
+
+
+
+
+
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
