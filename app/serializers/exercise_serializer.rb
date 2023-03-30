71c0bb22da0b2350c20c5b59c455600cd6545afa
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-03-30 16:04:18 +0300

    Finalizing the api

--- app/serializers/exercise_serializer.rb
+++ app/serializers/exercise_serializer.rb
@@ -1,4 +1,4 @@
 class ExerciseSerializer < ActiveModel::Serializer
   attributes :id, :name, :calories, :description, :workout_id
-  belongs_to :workout, serializer: :workout
+  belongs_to :workout
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :exercise_logs
   resources :exercises
   resources :workouts
-  resources :users
+  resources :users, only:[:index, :show, :update, :destroy]
+
+  post "/signup", to: 'users#create'
 
   post"/login", to: 'sessions#create'
   delete'/logout', to: 'sessions#destroy'
