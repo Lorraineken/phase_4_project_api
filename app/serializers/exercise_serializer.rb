3cbaec4ca8c5cb92c3eae2ebd6b98c1d09b759a0
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-03-30 13:14:46 +0300

    Corrected exercise relationship

--- app/serializers/exercise_serializer.rb
+++ app/serializers/exercise_serializer.rb
@@ -1,4 +1,4 @@
 class ExerciseSerializer < ActiveModel::Serializer
   attributes :id, :name, :calories, :description, :workout_id
-  belong_to :workout, serializer: :workout
+  belongs_to :workout, serializer: :workout
 end
