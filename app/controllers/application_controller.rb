54df100619ca80f00d7628c17e715c7aee26a250
Lorraineken <lorrainekupa@gmail.com>
2023-03-31 22:37:37 +0300

    Enabled cross_origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
+    configure do
+        enable :cross_origin
+      end
+
     def current_user
         @current_user = User.find_by(id: session[:user_id])
     end
